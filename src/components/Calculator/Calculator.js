319ee1412e921bbf41fb4dc42b76219695197d47
Junmning Guo <junming.guo@slalom.com>
2021-07-28 16:17:43 -0700

    Add markup and style for result section

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -8,7 +8,13 @@ const Calculator = () => {
           <label className="label" htmlFor="bill-input">
             Bill
           </label>
-          <input className="input" id="bill-input" type="number" min="0" max="1000000" />
+          <input
+            className="input"
+            id="bill-input"
+            type="number"
+            min="0"
+            max="1000000"
+          />
         </div>
         <div className="tip">
           <label className="label">Select Tip &#37;</label>
@@ -18,7 +24,11 @@ const Calculator = () => {
             <button className="tip-percent-btn">15&#37;</button>
             <button className="tip-percent-btn">25&#37;</button>
             <button className="tip-percent-btn">50&#37;</button>
-            <input className="custom-percent" type="number" placeholder="Custom" />
+            <input
+              className="custom-percent"
+              type="number"
+              placeholder="Custom"
+            />
           </div>
         </div>
         <div className="people">
@@ -28,7 +38,23 @@ const Calculator = () => {
           <input className="input" id="num-of-people" type="number" />
         </div>
       </section>
-      <section className="result-section"></section>
+      <section className="result-section">
+        <div className="tip-amount">
+          <label>
+            Tip Amount <br /> <span>/ person</span>
+          </label>
+          <label className="result-text">$4.23</label>
+        </div>
+        <div className="total">
+          <label>
+            Total <br /> <span>/ person</span>
+          </label>
+          <label className="result-text">$43.2</label>
+        </div>
+        <div className="reset">
+          <button className="reset-btn">Reset</button>
+        </div>
+      </section>
     </main>
   );
 };
