f8113dc04d4413dc21c6e0a1e8c306245184f9e5
Junmning Guo <junming.guo@slalom.com>
2021-07-27 17:22:38 -0700

    Add Header style

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,11 @@
 import './App.scss';
+import Header from '../Header/Header';
 
 function App() {
   return (
-    <div className="app"></div>
+    <div className="app">
+      <Header />
+    </div>
   );
 }
 
---
+++ src/components/Header/Header.js
@@ -0,0 +1,15 @@
+import "./Header.scss";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1 className="title">
+        spli
+        <br />
+        tter
+      </h1>
+    </header>
+  );
+};
+
+export default Header;
