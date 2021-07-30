c6f835152978b76535cb3b4a54f5498fc85ab8be
Junmning Guo <junming.guo@slalom.com>
2021-07-30 14:11:32 -0700

    Add method to handle tip change

--- src/components/TipInput/TipInput.js
+++ src/components/TipInput/TipInput.js
@@ -1,21 +1,37 @@
-const TipInput = ({ customTip, handleTipChange }) => {
+const TipInput = ({ tip, customTip, handleTipChange, handleCustomTipChange }) => {
+  const handleOnChange = (e) => {
+    if (e.target.type === "radio") {
+      handleCustomTipChange("");
+      handleTipChange(e.target.value);
+    }
+  }
+
+  const handleCustomPercentChange = (e) => {
+    handleTipChange("");
+    handleCustomTipChange(e.target.value.toString());
+  }
+
   return (
     <div className="tip">
       <label className="label">Select Tip &#37;</label>
-      <div className="tip-percent">
-        <button className="tip-percent-btn">5&#37;</button>
-        <button className="tip-percent-btn">10&#37;</button>
-        <button className="tip-percent-btn">15&#37;</button>
-        <button className="tip-percent-btn">25&#37;</button>
-        <button className="tip-percent-btn">50&#37;</button>
+      <div className="tip-percent" onChange={e => handleOnChange(e)}>
+        <input readOnly className="tip-input" type="radio" name="tip" value="5" id="5percent" checked={tip === "5"} />
+        <label className="tip-label" htmlFor="5percent">5&#37;</label>
+        <input readOnly className="tip-input" type="radio" name="tip" value="10" id="10percent" checked={tip === "10"} />
+        <label className="tip-label" htmlFor="10percent">10&#37;</label>
+        <input readOnly className="tip-input" type="radio" name="tip" value="15" id="15percent" checked={tip === "15"} />
+        <label className="tip-label" htmlFor="15percent">15&#37;</label>
+        <input readOnly className="tip-input" type="radio" name="tip" value="25" id="25percent" checked={tip === "25"} />
+        <label className="tip-label" htmlFor="25percent">25&#37;</label>
+        <input readOnly className="tip-input" type="radio" name="tip" value="50" id="50percent" checked={tip === "50"} />
+        <label className="tip-label" htmlFor="50percent">50&#37;</label>
         <input
           className="custom-percent"
           type="number"
           placeholder="Custom"
+          step="1"
           value={customTip}
-          onChange={(e) =>
-            handleTipChange(+Math.abs(e.target.value).toFixed(2) || 0)
-          }
+          onChange={(e) => handleCustomPercentChange(e)}
         />
       </div>
     </div>
