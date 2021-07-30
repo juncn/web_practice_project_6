ab396d5756f5ba8441f33ee5e7f2145242b5dcf9
Junmning Guo <junming.guo@slalom.com>
2021-07-30 14:15:04 -0700

    Update props passed into TipInput

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -8,30 +8,35 @@ import TotalAmount from "../TotalAmount/TotalAmount";
 import ResetButton from "../ResetButton/ResetButton";
 
 const Calculator = () => {
+  const DEFAULT_TIP = "15";
+  const DEFAULT_CUSTOM_TIP = "";
+
   const [bill, setBill] = useState(0);
   const [numOfPeople, setNumOfPeople] = useState(1);
-  const [tip, setTip] = useState(15);
-  const [customTip, setCustomTip] = useState("");
+  const [tip, setTip] = useState(DEFAULT_TIP);
+  const [customTip, setCustomTip] = useState(DEFAULT_CUSTOM_TIP);
   const [tipPerPerson, setTipPerPerson] = useState(0);
   const [totalPerPerson, setTotalPerPerson] = useState(0);
 
   useEffect(() => {
-    console.log(bill, typeof bill);
-    const finalTipPercent = customTip > 0 ? customTip : tip;
-    setTipPerPerson(
-      parseFloat((bill * (finalTipPercent / 100)) / numOfPeople).toFixed(2)
-    );
-  }, [bill, numOfPeople, tip, customTip]);
-
-  useEffect(() => {
-    const finalTotalPerPerson = parseFloat(
-      bill / numOfPeople + parseFloat(tipPerPerson)
-    ).toFixed(2);
-    setTotalPerPerson(finalTotalPerPerson);
-  }, [bill, numOfPeople, tipPerPerson]);
+    const finalTip = parseFloat(customTip) > 0 ? parseFloat(customTip) : parseFloat(tip);
+    const finalTipPerPerson = parseFloat(bill) * (finalTip / 100) / numOfPeople;
+    setTipPerPerson(finalTipPerPerson.toFixed(2));
+    
+    const finalTotalPerPerson = parseFloat(bill) / numOfPeople + parseFloat(tipPerPerson);
+    setTotalPerPerson(finalTotalPerPerson.toFixed(2));
+  }, [bill, numOfPeople, tip, customTip, tipPerPerson]);
 
   const handleTipChange = (newTip) => {
     setTip(newTip);
+  };
+
+  const handleCustomTipChange = (newCustomTip) => {
+    if (newCustomTip) {
+      setCustomTip(Math.abs(Math.round(newCustomTip)).toString());
+    } else {
+      setCustomTip(DEFAULT_CUSTOM_TIP);
+    }
   };
 
   const handleBillChange = (newBill) => {
@@ -45,15 +50,20 @@ const Calculator = () => {
   const handleReset = () => {
     setBill(0);
     setNumOfPeople(1);
-    setTip(15);
-    setCustomTip("");
+    setTip(DEFAULT_TIP);
+    setCustomTip(DEFAULT_CUSTOM_TIP);
   };
 
   return (
     <main>
       <section className="input-section">
         <BillInput bill={bill} handleBillChange={handleBillChange} />
-        <TipInput customTip={customTip} handleTipChange={handleTipChange} />
+        <TipInput
+          tip={tip}
+          customTip={customTip}
+          handleTipChange={handleTipChange}
+          handleCustomTipChange={handleCustomTipChange}
+        />
         <PeopleInput
           numOfPeople={numOfPeople}
           handleNumOfPeopleChange={handleNumOfPeopleChange}
